1f608211e7e9c9a8b7442b2b1a0dc388f37189bd
csarsound <csarsound@hotmail.com>
2022-09-09 18:41:07 -0500

    HowManySubs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
-""" Queries the Reddit API and prints the
-titles of the first 10 hot posts listed for a given subreddit. """
+""" Queries the Reddit API and returns the
+number of subscribers for a given subreddit. """
 
 import requests
 headers = {"User-Agent": "ubuntu:hbtn:v1.0 (by /u/Brandixitor)"}
 
 
-def top_ten(subreddit):
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+def number_of_subscribers(subreddit):
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     request = requests.get(url, headers=headers, allow_redirects=False)
 
     if request.status_code == 200:
-        for children in request.json().get("data").get("children"):
-            print(children.get("data").get("title"))
+        return request.json().get("data").get("subscribers")
 
-    else:
-        print(None)
+    return 0
